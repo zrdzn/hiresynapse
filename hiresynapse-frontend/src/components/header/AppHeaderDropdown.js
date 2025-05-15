a1c833d387570c84ba03f51945687ea668720982
zrdzn <zrdzn9@gmail.com>
2025-05-15 20:44:54 +0200

    Modify account dropdown

--- hiresynapse-frontend/src/components/header/AppHeaderDropdown.js
+++ hiresynapse-frontend/src/components/header/AppHeaderDropdown.js
@@ -1,25 +1,6 @@
 import React from 'react'
-import {
-  CAvatar,
-  CBadge,
-  CDropdown,
-  CDropdownDivider,
-  CDropdownHeader,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
-} from '@coreui/react'
-import {
-  cilAccountLogout,
-  cilBell,
-  cilCommentSquare,
-  cilCreditCard,
-  cilEnvelopeOpen,
-  cilFile,
-  cilSettings,
-  cilTask,
-  cilUser,
-} from '@coreui/icons'
+import {CAvatar, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle,} from '@coreui/react'
+import {cilAccountLogout,} from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
 const AppHeaderDropdown = ({ authDetails }) => {
@@ -29,60 +10,7 @@ const AppHeaderDropdown = ({ authDetails }) => {
         <CAvatar src={authDetails.pictureUrl ?? authDetails.username} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilBell} className="me-2" />
-          Updates
-          <CBadge color="info" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilEnvelopeOpen} className="me-2" />
-          Messages
-          <CBadge color="success" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilTask} className="me-2" />
-          Tasks
-          <CBadge color="danger" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCommentSquare} className="me-2" />
-          Comments
-          <CBadge color="warning" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Settings</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilUser} className="me-2" />
-          Profile
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilSettings} className="me-2" />
-          Settings
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCreditCard} className="me-2" />
-          Payments
-          <CBadge color="secondary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilFile} className="me-2" />
-          Projects
-          <CBadge color="primary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem href="http://localhost:8080/logout">
           <CIcon icon={cilAccountLogout} className="me-2" />
           Logout
         </CDropdownItem>
